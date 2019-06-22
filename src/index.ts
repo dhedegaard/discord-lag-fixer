f190d83d9fd8926c99af52beedc86d6330fd090e
Dennis Hedegaard <dennis@dhedegaard.dk>
2019-06-22 12:35:51 +0200

    Added logic to avoid hardcoding the regions to swap between, made the change message prettier, various docs stuff, added some logging.

--- src/index.ts
+++ src/index.ts
@@ -2,35 +2,74 @@ import { config } from "dotenv";
 config();
 import * as Discord from "discord.js";
 
-const REGIONS = Object.freeze(["eu-west", "eu-central"]);
+const REGIONS_PREFIX = process.env.REGIONS_PREFIX;
+if (typeof REGIONS_PREFIX !== 'string' || REGIONS_PREFIX.length < 1){
+ throw new Error('Missing REGIONS_PREFIX setting')
+} ))
 
-let lastChange = new Date();
+let lastChange = new Date(0);
+
+const log = (...args: any[]) => {
+  console.log(`[${new Date().toUTCString()}]:`, ...args);
+};
 
 const main = async () => {
   const client = new Discord.Client();
   await client.login(process.env.TOKEN);
-  console.log("Logged in successfully");
+  log("Logged in successfully");
+  log("");
 
-  client.on("message", message => {
+  // List the current guilds.
+  log("Current list of guilds:");
+  log("");
+  client.guilds.forEach(guild => log("-", guild.name, `(${guild.id})`));
+  log("");
+
+  // Log various interesting events.
+  client.on("disconnect", () => log("Disconnected"));
+  client.on("reconnecting", () => log("Reconnecting"));
+
+  // Handle messages.
+  client.on("message", async message => {
+    // Check if we should ignore the message.
     if (
-      !message.author.bot &&
-      /^.*fix +lag.*$/i.test(message.content.toLowerCase())
+      message.author.bot ||
+      !/^.*fix +lag.*$/i.test(message.content.toLowerCase())
     ) {
-      // Check that at least 5 seconds have passed since the last change.
-      if (lastChange.getTime() + 5000 > new Date().getTime()) {
-        message.reply("Not so fast :)");
-        return;
-      }
-
-      // Change voice region.
-      const currentRegion = message.guild.region;
-      const newRegion = REGIONS.filter(e => e !== currentRegion)[0];
-      message.guild.setRegion(newRegion);
-
-      // Notify the caller, and update the timestamp.
-      message.reply(`Moved from **${currentRegion}** to **${newRegion}**`);
-      lastChange = new Date();
+      return;
     }
+
+    // Check that at least 5 seconds have passed since the last change.
+    if (lastChange.getTime() + 5000 > new Date().getTime()) {
+      message.reply("Not so fast :)");
+      return;
+    }
+
+    // Fetch the current voice regions and determine the current one.
+    const regions = (await message.guild.fetchVoiceRegions())
+      .filter(e => e.id.startsWith(REGIONS_PREFIX))
+      .map(e => e); // Make sure we have an array.
+    const currentRegion = regions.find(e => e.id === message.guild.region);
+
+    // Determine the new region to go to.
+    const newRegions = regions.filter(e => e.id !== currentRegion.id);
+    const newRegion = newRegions[Math.floor(Math.random() * newRegions.length)]
+    if (newRegion == null) {
+      throw new Error(`No new region to go to: ${JSON.stringify(regions)}`);
+    }
+    // Change the voice region.
+    message.guild.setRegion(newRegion.id);
+
+    // Notify the caller, and update the timestamp.
+    const currentRegionLabel =
+      currentRegion != null
+        ? `${currentRegion.name} (${currentRegion.id})`
+        : message.guild.region;
+    const newRegionLabel = `${newRegion.name} (${newRegion.id})`;
+    message.reply(
+      `Moved from **${currentRegionLabel}** to **${newRegionLabel}**`
+    );
+    lastChange = new Date();
   });
 };
 
